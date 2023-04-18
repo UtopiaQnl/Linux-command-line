005dd0393bbc553c7274d1481d11e5f9277cfd36
Ivan Covach <eeiprex@gmail.com>
2023-04-18 22:20:16 +0300

    add index.php

---
+++ chapter_16/index.php
@@ -0,0 +1,107 @@
+
+    <!DOCTYPE HTML>
+    <html lang="en">
+
+    <head>
+
+    <title>LinuxCommand.org: Learn The Linux Command Line. Write Shell Scripts.</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="description" content="LinuxCommand.org is a web site that helps users discover the power of the Linux command line.">
+    <meta name="keywords" content="Linux command line, Linux shell scripting, GNU coreutils, Linux console, bash shell, bash shell scripting, Linux CLI">
+    <meta name="author" content="William Shotts, Jr.">
+    <meta name="copyright" content="Copyright 2000-2023, William Shotts, Jr.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" type="text/css" href="linuxcommand5.css">
+    </head>
+
+    <body>
+
+    <div class="grid-container">
+    <div class="colorblock-red">
+    </div><!-- colorblock-red -->
+    <div class="title-img">
+      <img src="images/lc3_logo_1-5.png" width="792" height="115" alt="Title graphic">
+    </div><!-- title-img -->
+    <div class="colorblock-white">
+    <div class="navbar">
+      <ul style="line-height: 150%;">
+        <li><a href="index.php">Home</a></li>
+        <li><a href="lc3_learning_the_shell.php">Learning&nbsp;the&nbsp;Shell</a></li>
+        <li><a href="lc3_writing_shell_scripts.php">Writing&nbsp;Shell&nbsp;Scripts</a></li>
+        <li><a href="lc3_resources.php">Resources</a></li>
+        <li><a href="tlcl.php">Books</a></li>
+        <li><a href="lc3_adventures.php">Adventures</a></li>
+      </ul>
+      <hr>
+      <ul>
+        <li><a href="https://lcorg.blogspot.com">Blog</a></li>
+      </ul>
+    </div><!-- navbar -->
+  
+    </div><!-- colorblock-white -->
+
+    <div id="top" class="main">
+<img src="images/Screenshot-Terminal.png"
+  alt="terminal display"
+  width="660"
+  height="442">
+    
+<h1>Now what?</h1>
+    
+<p>You have Linux installed and running. The GUI is working fine, but you are
+getting tired of changing your desktop themes. You keep seeing this "terminal"
+thing.<br> <br>
+
+Don't worry, we'll show you <a href="lc3_learning_the_shell.php">what to do</a>.</p>
+
+
+    <div class="navbar2">
+      <ul style="line-height: 150%;">
+        <li><a href="lc3_learning_the_shell.php">Learning&nbsp;the&nbsp;Shell</a></li>
+        <li><a href="lc3_writing_shell_scripts.php">Writing&nbsp;Shell&nbsp;Scripts</a></li>
+        <li><a href="lc3_resources.php">Resources</a></li>
+        <li><a href="tlcl.php">Books</a></li>
+        <li><a href="lc3_adventures.php">Adventures</a></li>
+      </ul>
+      <hr>
+      <ul>
+        <li><a href="http://lcorg.blogspot.com">Blog</a></li>
+      </ul>
+    </div><!-- navbar2 -->
+  
+<hr>
+<div style="overflow: auto">
+  <img src="images/linuxcommandline.png"
+    alt="book cover"
+    width="87"
+    height="115"
+    style="float: left; padding-right: 5px;">
+  <p><span style="font-size:125%">
+    Like the site? You'll love the book!<br>
+  </span>
+  <i><a href="tlcl.php">The Linux Command Line</a></i> by William Shotts</p>
+</div>
+<hr>
+<a href="https://sourceforge.net/projects/linuxcommand">
+  <img style="width: 120px; height: 30px; display: block; margin-left: auto; margin-right: auto"
+    src="https://sflogo.sourceforge.net/sflogo.php?group_id=3268&amp;type=11"
+    alt="Get LinuxCommand at SourceForge.net. Free Open Source software downloads">
+</a>
+<a rel="me" href="https://mstdn.social/@william_shotts"></a>
+
+
+<div class="footer">
+<hr>
+<p class="fineprint">
+&copy; 2000-2023,
+<a href="mailto:bshotts@users.sourceforge.net">William E. Shotts, Jr.</a>
+Verbatim copying and distribution of this entire article is
+permitted in any medium, provided this copyright notice is preserved.</p>
+<p class="fineprint">Linux&reg; is a registered trademark of Linus Torvalds.</p>
+</div><!-- footer -->
+</div><!-- main -->
+</div><!-- grid-container -->
+</body>
+
+</html>
