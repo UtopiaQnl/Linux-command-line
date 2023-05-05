6fd9c6d692a9e1d45607ed08e3380bc615441045
Ivan Covach <eeiprex@gmail.com>
2023-05-05 16:04:07 +0300

    [chapter 28] add read-multiple.sh

---
+++ chapter_25-36/read-multiple.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo -n "Enter ont or more value -> "
+read -r var1 var2 var3 var4 var5
+
+echo "var1 = '$var1'"
+echo "var2 = '$var2'"
+echo "var3 = '$var3'"
+echo "var4 = '$var4'"
+echo "var5 = '$var5'"
