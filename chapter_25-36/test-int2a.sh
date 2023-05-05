ee9efe90305a766c10e0ec6c1d69794840e96f38
Ivan Covach <eeiprex@gmail.com>
2023-05-05 14:58:40 +0300

    [chapter 27] add test-int2a.sh

---
+++ chapter_25-36/test-int2a.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+INT=21438
+
+if [[ "$INT" =~ ^(\+|-)?([1-9][0-9]*|0)$ ]]; then
+    if ((INT == 0)); then
+        echo "INT is zero."
+    else
+        if ((INT < 0)); then
+            echo "INT is negative."
+        else
+            echo "INT is positive."
+        fi
+        if (( (INT % 2) == 0)); then
+            echo "INT is even."
+        else
+            echo "INT is odd."
+        fi
+    fi
+else
+    echo "INT is not a integer." >&2
+    exit 1
+fi
