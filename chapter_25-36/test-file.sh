83e57794c908be6fb3ccc529b2ce27483cb70bbb
Ivan Covach <eeiprex@gmail.com>
2023-05-05 13:59:56 +0300

    [chapter 27] init test (if conditions)

---
+++ chapter_25-36/test-file.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+FILE="$HOME/.bashrc"
+
+if [ -e "$FILE" ]; then
+    if [ -f "$FILE" ]; then
+        echo "$FILE is a regular file."
+    fi
+    if [ -d "$FILE" ]; then
+        echo "$FILE is a directory."
+    fi
+    if [ -r "$FILE" ]; then
+        echo "$FILE is readble."
+    fi
+    if [ -w "$FILE" ]; then
+        echo "$FILE is writable."
+    fi
+    if [ -x "$FILE" ]; then
+        echo "$FILE is executable/searchable."
+    fi
+else
+    echo "$FILE does not exist"
+    exit 1
+fi
+exit
