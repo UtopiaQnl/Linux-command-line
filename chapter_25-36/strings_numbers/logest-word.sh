be3bdb754d82fd013f27adad7ea1828cb402851c
Ivan Covach <eeiprex@gmail.com>
2023-05-11 17:04:05 +0300

    [chore] изменени прав

--- chapter_25-36/strings_numbers/logest-word.sh
+++ chapter_25-36/strings_numbers/logest-word.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
 for i; do
-    if [[ -r "$i" ]]; then
-        max_word=
+    if [[ -n "$i" ]]; then
         max_len=0
+        max_word=
         for j in $(strings "$i"); do
-            # shellcheck disable=SC2000
-            len="$(echo "$j" | wc -c)"
-            if ((len > max_len)); then
-                max_word="$j"
+            len="${#j}"
+            if (( len > max_len )); then
                 max_len="$len"
+                max_word="$j"
             fi
         done
         echo "$i: '$max_word' ($max_len characters)"
